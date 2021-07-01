b13c155099d0603b157776502981eadc67914fff
Vinícius Lucena <jviniiciuslucena@gmail.com>
2021-07-01 14:58:12 -0300

    Segundo dia Origin finalizado :rocket

---
+++ Origin/main.js
@@ -0,0 +1,30 @@
+// Toggle Menu
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for(const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show');
+    })
+}
+
+// Esconder Menu ao clicar em um link
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show');
+    })
+}
+
+// Sombra do header ao scroll
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+    if(this.window.scrollY >= navHeight) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+})
